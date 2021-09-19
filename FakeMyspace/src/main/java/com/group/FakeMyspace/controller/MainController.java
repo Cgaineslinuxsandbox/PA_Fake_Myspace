d35428d284c8a767e3d791ede4509d790c559a6d
Jesus <Jvillalba23@yahoo.com>
2021-09-19 11:15:04 -0500

    message

--- FakeMyspace/src/main/java/com/group/FakeMyspace/controller/MainController.java
+++ FakeMyspace/src/main/java/com/group/FakeMyspace/controller/MainController.java
@@ -12,7 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+import com.Jesus.vegan.models.Food;
+import com.group.FakeMyspace.models.Message;
 import com.group.FakeMyspace.models.User;
 import com.group.FakeMyspace.services.UServ;
 
@@ -83,6 +84,36 @@ public class MainController {
 	    }
 	
 	
-	
+
+	    
+	    
+	    
+	    //=============================Post Mapping to the Message. Needs a link to this in the profile.jsp====================================//
+	    public Long userSessionId(HttpSession session) {
+			if(session.getAttribute("user___Id") == null) {
+				return null;
+			}
+			return (Long)session.getAttribute("user___Id");
+		}
+	    
+	    @PostMapping("/main/{id}/message") 
+		public String Create(@Valid @ModelAttribute("message") Message message, BindingResult result, Model viewModel, HttpSession session, @RequestParam("name") String name, @RequestParam("content") String content) {
+			if(result.hasErrors()) {
+				Long userId = this.userSessionId(session);
+				User user = this.uServ.findUserById(userId);
+				viewModel.addAttribute("user", user);
+				return "profile.jsp";
+			}
+			Long userId = this.userSessionId(session);
+			User user = this.uServ.findUserById(userId);
+			message.setSender(user);
+			this.Create(message, result, viewModel, session, name, content);
+			return "redirect:/main/{id}/message/sent";
+		}
+	    
+	    @GetMapping("/main/{id}/message/sent")
+	    public String Sent() {
+	    	return "sent.jsp";
+	    }
 	
 }
